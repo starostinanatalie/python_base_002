771e3477f7238e7d04297ae98b20f097480ea8e7
starostinanatalie <starostina.natalie@gmail.com>
2020-12-27 21:38:36 +0300

    Lesson 2 task 1 Starostina.Natalie

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -3,6 +3,8 @@
 
 # Есть словарь координат городов
 
+from pprint import pprint
+
 sites = {
     'Moscow': (550, 370),
     'London': (510, 510),
@@ -13,7 +15,22 @@ sites = {
 # расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 distances = {}
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
+moscow_to_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2) ** 0.5
+moscow_to_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2) ** 0.5
+paris_to_london = ((paris[0] - london[0]) ** 2 + (paris[1] - london[1]) ** 2) ** 0.5
 
-# TODO здесь заполнение словаря
+distances['Moscow'] = {}
+distances['Moscow']['London'] = moscow_to_london
+distances['Moscow']['Paris'] = moscow_to_paris
+distances['London'] = {}
+distances['London']['Moscow'] = moscow_to_london
+distances['London']['Paris'] = paris_to_london
+distances['Paris'] = {}
+distances['Paris']['Moscow'] = moscow_to_paris
+distances['Paris']['London'] = paris_to_london
+
 
 print(distances)
