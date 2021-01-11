6717147e6a9eb91bfd30fa7d6d59824d50a29ff7
michael <chebotarev.michael@yandex.ru>
2021-01-11 17:50:55 +0300

    lesson 002 - нет: 09; почти: 00,01,05; да: ост. (сил.)

--- lesson_002/02_operations.py
+++ lesson_002/02_operations.py
@@ -13,3 +13,4 @@ print(result)
 answer = 1 * (-2 + 3 + 4) * 5
 print(answer)
 
+# зачет!
--- lesson_002/07_secret.py
+++ lesson_002/07_secret.py
@@ -33,3 +33,5 @@ secret_message = [
 #   Если нужны вычисления и разные пробы - делайте это в консоли пайтона, тут нужен только результат
 
 print(secret_message[0][3], secret_message[1][9:13], secret_message[2][5:15:2], secret_message[3][12:6:-1], secret_message[4][20:15:-1])
+
+# зачет!
