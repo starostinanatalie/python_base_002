52ccff2350621cc6e6f8506a2351e474ace55c7a
starostinanatalie <starostina.natalie@gmail.com>
2021-02-11 15:55:59 +0300

    Lesson 3 task 8 Starostina update

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -6,7 +6,15 @@ import simple_draw
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+#
+for y in range(0, 600, 50):
+    slide_x = y % 100
+    for x in range(slide_x, 1200, 100):
+        left_bottom = simple_draw.get_point(x, y)
+        right_top = simple_draw.get_point(x + 100, y + 50)
+        simple_draw.rectangle(left_bottom=left_bottom, right_top=right_top, color=[216,53,225], width=1)
+
+
 # Подсказки:
 #  Для отрисовки кирпича использовать функцию rectangle
 #  Алгоритм должен получиться приблизительно такой:
