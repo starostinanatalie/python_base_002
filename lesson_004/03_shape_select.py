ca090e56fce80f1829e3c7728ab8196fa8aa4d39
starostinanatalie <starostina.natalie@gmail.com>
2021-04-18 00:39:59 +0300

    Lesson 4 task 3 update and bugfix - Starostina

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -19,23 +19,23 @@ def draw_polygon(angles_quantity, initial_point, angle, length, width, colour):
         angle = angle + (360 / angles_quantity)
     sd.line(side.end_point, end_point=initial_point, color=colour, width=width)
 
-def draw_triangle(initial_point, angle, side_length):
+def draw_triangle(initial_point, angle, side_length, colour):
     angles_quantity = 3
     draw_polygon(angles_quantity, initial_point, angle, side_length, 3, colour)
 
-def draw_square(initial_point, angle, side_length):
+def draw_square(initial_point, angle, side_length, colour):
     angles_quantity = 4
     draw_polygon(angles_quantity, initial_point, angle, side_length, 3, colour)
 
-def draw_pentagon(initial_point, angle, side_length):
+def draw_pentagon(initial_point, angle, side_length, colour):
     angles_quantity = 5
     draw_polygon(angles_quantity, initial_point, angle, side_length, 3, colour)
 
-def draw_hexagon(initial_point, angle, side_length):
+def draw_hexagon(initial_point, angle, side_length,colour):
     angles_quantity = 6
     draw_polygon(angles_quantity, initial_point, angle, side_length, 3, colour)
 
-def draw_septagon(initial_point, angle, side_length):
+def draw_septagon(initial_point, angle, side_length, colour):
     angles_quantity = 7
     draw_polygon(angles_quantity, initial_point, angle, side_length, 3, colour)
 
@@ -53,8 +53,8 @@ choise_colour = 0
 
 while True:
     print("Выберите цвет, которым Вы хотите нарисовать фигуру, из предложенных:")
-    for i, (name, colour) in enumerate(colours):
-        print(i + 1, name)
+    for i, (name, colour) in enumerate(colours, 1):
+        print(i, name)
     choise_colour = int(input("Введите номер выбранного цвета: "))
     if choise_colour >= len(colours) + 1:
         print('Вы ввели некорректное значение')
@@ -62,7 +62,9 @@ while True:
         break
 colour = colours[choise_colour - 1][1]
 
-point = sd.get_point(100,100)
+x = (sd.resolution[0] - side_length) / 2
+y = (sd.resolution[1] - side_length * 2) / 2
+point = sd.get_point(x, y)
 
 polygons = [("Треугольник", draw_triangle),
            ("Квадрат", draw_square),
@@ -77,21 +79,19 @@ choise_figure = 0
 
 while True:
     print("Выберите фигуру, которую Вы хотите нарисовать, из предложенных: ")
-    for i, (name, figure) in enumerate(polygons, 1):
-        print(i + 1, name)
-        choise_figure = int(input("Введите номер выбранного цвета: "))
-    if choise_figure >= len(polygons):
+    for i, figure in enumerate(polygons, 1):
+        print(i, figure[0])
+    choise_figure = int(input("Введите номер выбранной фигуры: "))
+    if choise_figure >= len(polygons) + 1:
         print('Вы ввели некорректное значение')
     else:
         break
 
-
-x = (sd.resolution[0] - side_length) / 2
-delta_y = (side_length / (2 * math.sin(math.pi / (choise_figure + 2)))) * 2
+delta_y = (side_length / (2 * math.sin(math.pi / (choise_figure + 1)))) * 2
 y = (sd.resolution[1] - int(delta_y)) / 2
 point = sd.get_point(x, y)
 
-polygons[choise_figure - 1][1](point, 0, side_length)
+polygons[choise_figure - 1][1](point, 0, side_length, colour)
 
 sd.pause()
 
