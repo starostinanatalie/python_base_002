a701eee5a4cb9639cc744d2ce0bb361c34ea4818
starostinanatalie <starostina.natalie@gmail.com>
2021-08-21 01:21:05 +0300

    2 version - Starostina

--- lesson_006/mastermind_engine.py
+++ lesson_006/mastermind_engine.py
@@ -4,18 +4,32 @@ number_list = []
 
 def get_number():
     global number_list
-    number = randint(1000,9999)
-    divider = 1000
-    for _ in range(4):
-        number_list.append(number // divider)
-        number = int(number % divider)
-        divider = int(divider // 10)
-
-
-
-
+    while True:
+        number_list.clear()
+        number = randint(1000,9999)
+        divider = 1000
+        for _ in range(4):
+            number_list.append(number // divider)
+            number = int(number % divider)
+            divider = int(divider // 10)
+        prove_set = set(number_list)
+        if len(prove_set) == 4:
+            break
 
 def check_number(number):
-    pass
+    divider = 1000
+    answer = {'Bulls': 0, 'Cows': 0}
+    for i in range(4):
+        if int(number // divider) == number_list[i]:
+            answer['Bulls'] += 1
+        elif int(number // divider) in number_list:
+            answer['Cows'] += 1
+        number = int(number % divider)
+        divider = int(divider // 10)
+    return answer
 
-get_number()
+
+
+
+
+
